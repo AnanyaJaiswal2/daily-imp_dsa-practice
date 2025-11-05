b5c57e5e17fe0993d4efee951eec816101558871
Ananya Jaiswal <ananyajaiswal518@gmail.com>
2025-11-05 13:43:43 +0530

    daily update

---
+++ Tree/preorder.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    Node(int val){
+        data=val;
+        left=right=NULL;
+    }
+};
+void preorder(Node* root){
+    if(root==NULL) return;
+    cout<<(root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+int main() {
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+
+    preorder(root);
+    return 0;
+}
